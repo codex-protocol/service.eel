d48ad0affaed583dd4a666a76f9d53d61d908a9d
Colin Wood <colinjameswood@gmail.com>
2018-09-25 16:05:17 -0500

    Updates configs and deployment scripts to support new network-based deployment strategy

--- deployment/secrets-to-env.sh
+++ deployment/secrets-to-env.sh
@@ -3,7 +3,7 @@
 EC2_AVAILABILITY_ZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
 EC2_REGION=$(echo $EC2_AVAILABILITY_ZONE | sed 's/[a-z]$//')
 
-aws secretsmanager get-secret-value --secret-id eel --region $EC2_REGION |
+aws secretsmanager get-secret-value --region $EC2_REGION --secret-id "$DEPLOYMENT_GROUP_NAME.eel" |
 grep 'SecretString' |
 sed -E $'s/.*\{\\\\"(.*)\\\\"\}.*/\\1/' |
 sed -E $'s/\\\\":\\\\"/=/g' |
---
+++ src/config/index.js
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv'
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+
+// reads environment variables stored in the '.env' file and writes them to the
+//  process.env object
+const dotenvResult = dotenv.config({ path: `${__dirname}/../../.env` })
+
+if (dotenvResult.error) {
+  throw dotenvResult.error
+}
+
+const commonConfig = {
+  mongodbUri: encodeURI(process.env.MONGODB_URI), // NOTE: encodeURI is necessary for passwords with URI reserved characters
+
+  // report errors to sentry if a DSN is specified
+  useSentry: !!process.env.SENTRY_DSN,
+
+  // log events to mixpanel if a token is specified
+  useMixpanel: !!process.env.MIXPANEL_TOKEN,
+}
+
+const networkName = (() => {
+  switch (process.env.ETHEREUM_NETWORK_ID) {
+    case '1': return 'mainnet'
+    case '3': return 'ropsten'
+    case '4': return 'rinkeby'
+    case '5777': return 'ganache'
+    default: throw new Error(`network with id ${process.env.ETHEREUM_NETWORK_ID} is invalid, no network name defined in config`)
+  }
+})()
+
+// eslint-disable-next-line import/no-dynamic-require, global-require
+const networkConfig = require(`${__dirname}/${networkName}`).default
+
+export default Object.assign({ networkName }, networkConfig, commonConfig)
