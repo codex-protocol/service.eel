833633c92eee10c90ba60b89f1e9c18ed7c38fff
John Forrest <johnhforrest@outlook.com>
2018-10-16 14:31:02 -0500

    Reducing the block confirmations for ropsten and rinkeby

--- src/config/rinkeby.js
+++ src/config/rinkeby.js
@@ -5,7 +5,7 @@ export default {
   blockchain: {
     startingBlockHeight: 3069600,
     averageBlockTime: 15, // in seconds, this dictates how frequently to run agenda jobs
-    minConfirmations: 8,
+    minConfirmations: 1,
     chunkSize: 50000, // max number of blocks to request each time the process-blocks job is run
   },
 }
--- src/config/ropsten.js
+++ src/config/ropsten.js
@@ -5,7 +5,7 @@ export default {
   blockchain: {
     startingBlockHeight: 4132900,
     averageBlockTime: 15, // in seconds, this dictates how frequently to run agenda jobs
-    minConfirmations: 5,
+    minConfirmations: 1,
     chunkSize: 50000, // max number of blocks to request each time the process-blocks job is run
   },
 }
