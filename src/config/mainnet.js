dcd33dcdb7a42ec230f719fa41a023d2ac7da3c4
John Forrest <johnhforrest@outlook.com>
2018-11-05 18:27:13 -0600

    Reduce mainnet confirmations to 2

--- src/config/mainnet.js
+++ src/config/mainnet.js
@@ -5,7 +5,7 @@ export default {
   blockchain: {
     startingBlockHeight: 6000000,
     averageBlockTime: 15, // in seconds, this dictates how frequently to run agenda jobs
-    minConfirmations: 8,
+    minConfirmations: 2,
     chunkSize: 50000, // max number of blocks to request each time the process-blocks job is run
   },
 }
