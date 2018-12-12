6bef501584281f6dbfb80fbc8cf56f9b30e6ea20
Colin Wood <colinjameswood@gmail.com>
2018-12-12 10:37:30 -0600

    Fixes secrects-to-env.sh bug that caused secret values ending in { to break the parsing

--- deployment/secrets-to-env.sh
+++ deployment/secrets-to-env.sh
@@ -5,6 +5,6 @@ EC2_REGION=$(echo $EC2_AVAILABILITY_ZONE | sed 's/[a-z]$//')
 
 aws secretsmanager get-secret-value --region $EC2_REGION --secret-id "$DEPLOYMENT_GROUP_NAME.eel" |
 grep 'SecretString' |
-sed -E $'s/.*\{\\\\"(.*)\\\\"\}.*/\\1/' |
+sed -E $'s/[^{]*\{\\\\"(.*)\\\\"\}[^}]*/\\1/' |
 sed -E $'s/\\\\":\\\\"/=/g' |
 sed -E $'s/\\\\",\\\\"/\\\n/g' > /opt/process/.env
