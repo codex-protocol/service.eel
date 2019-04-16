0b2645cd2ca696f60958b536c881c78cc21a34a4
Colin Wood <colinjameswood@gmail.com>
2019-04-16 11:15:29 -0500

    Upgrades to web3@1.0.0-beta.52 via ethereum-service@2.0.0

--- src/config/mainnet.js
+++ src/config/mainnet.js
@@ -1,11 +1,13 @@
+import { web3 } from '@codex-protocol/ethereum-service'
+
 export default {
   process: {
     logLevel: 'info',
   },
   blockchain: {
     startingBlockHeight: 6000000,
+    minConfirmations: web3.transactionConfirmationBlocks,
     averageBlockTime: 15, // in seconds, this dictates how frequently to run agenda jobs
-    minConfirmations: 2,
     chunkSize: 50000, // max number of blocks to request each time the process-blocks job is run
   },
 }
--- src/config/ropsten.js
+++ src/config/ropsten.js
@@ -1,11 +1,13 @@
+import { web3 } from '@codex-protocol/ethereum-service'
+
 export default {
   process: {
     logLevel: 'verbose',
   },
   blockchain: {
     startingBlockHeight: 4132900,
+    minConfirmations: web3.transactionConfirmationBlocks,
     averageBlockTime: 15, // in seconds, this dictates how frequently to run agenda jobs
-    minConfirmations: 1,
     chunkSize: 50000, // max number of blocks to request each time the process-blocks job is run
   },
 }
